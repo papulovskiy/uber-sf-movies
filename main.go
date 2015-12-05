bcc3e5e7ff525f1e8186b92f76eaf756c8828e71
Alexey Papulovskiy <a@c17.nl>
2015-12-05 19:29:15 +0100

    Cleanup

--- main.go
+++ main.go
@@ -17,14 +17,13 @@ func csv2database() *db {
 	if err != nil {
 		log.Fatal("Cannot open data file: %+v\n", err)
 	}
-	// log.Printf("%+v", list)
+
 	r := csv.NewReader(strings.NewReader(string(list)))
 	records, err := r.ReadAll()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// log.Printf("%+v\n", len(records))
 	database := new(db)
 	database.importArray(records)
 	return database
@@ -39,11 +38,9 @@ func main() {
 	router, err := rest.MakeRouter(
 		// rest.Get("/", index),
 		rest.Get("/places", getAllPlaces),
-		// rest.Get("/geo/by_movie/:id", getPlacesByMovie),
-		// rest.Get("/geo/by_company/:id", getPlacesByCompany),
-		// rest.Get("/geo/by_person/:id", getPlacesByPerson),
-		// rest.Put("/users/:id", users.PutUser),
-		// rest.Delete("/users/:id", users.DeleteUser),
+		// rest.Get("/places/by_movie/:id", getPlacesByMovie),
+		// rest.Get("/places/by_company/:id", getPlacesByCompany),
+		// rest.Get("/places/by_person/:id", getPlacesByPerson),
 	)
 	if err != nil {
 		log.Fatal(err)
@@ -51,18 +48,15 @@ func main() {
 	api.SetApp(router)
 	http.Handle("/api/", http.StripPrefix("/api", api.MakeHandler()))
 	http.Handle("/", http.FileServer(http.Dir("./static/")))
-	// http.Handle("/", http.StripPrefix("/static", http.FileServer(http.Dir("."))))
-	// http.FileServer(http.Dir("/usr/share/doc"))
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func getAllPlaces(w rest.ResponseWriter, r *rest.Request) {
 	places := make([]*Place, 0)
+	// TODO: fix to avoid copying on every request
 	for _, p := range database.places {
 		places = append(places, p)
-		log.Printf("Place: %+v\n", *p)
-		// p[i] = interface{label: p.name, lat: p.latitude, lon: p.longitude}
 	}
-	log.Printf("Places: \n%+v\n%+v\n", database.places, places)
 	w.WriteJson(&places)
 }
